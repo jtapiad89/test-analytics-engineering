9a30ceb9c8abc130b3c826a2e895795be09d73b2
jtapiad <jtapiad@bch.bancodechile.cl>
2024-10-09 12:34:37 -0300

    eliminacion de secreto

--- decrypt_shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/fresh-catwalk-438015-b4-d4954f42b87e.json fresh-catwalk-438015-b4-d4954f42b87e.json.gpg
-
