362f601192d27e7fbc9943473e325a3b1abf4d69
jtapiad <jtapiad@bch.bancodechile.cl>
2024-10-09 12:26:05 -0300

    add action

--- decrypt_shell.sh
+++ decrypt_shell.sh
@@ -4,6 +4,6 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="dataanalitycs" \
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $HOME/secrets/fresh-catwalk-438015-b4-d4954f42b87e.json fresh-catwalk-438015-b4-d4954f42b87e.json.gpg
 
